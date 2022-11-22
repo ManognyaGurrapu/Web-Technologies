6efc6199572a2480df8775706b88b2c7916b30ae
ManognyaGurrapu <106607621+ManognyaGurrapu@users.noreply.github.com>
2022-11-22 13:40:39 +0530

    Add files via upload

---
+++ Strings.js
@@ -0,0 +1,3 @@
+let s='keshav memorial institute of technology'
+console.log(s.substring(1,4));
+console.log(s.slice(1,13));
---
+++ first.js
@@ -0,0 +1,15 @@
+/*var a=10;
+var b=20;
+var z=a+b;
+console.log(z);
+console.log("Welcome to Javascript");
+var x="Hi";
+console.log(x)*/
+function add(){
+    let a=10;
+    var b=20;
+    console.log(a+b);
+}
+add();
+var a="Hello";
+console.log(a);
